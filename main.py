01906b78b969752c10ebe4203f78711f04ce7c0f
Stefan Etzkorn <stefanetzkorn91@gmail.com>
2016-07-12 17:58:43 +0200

    -

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import os
+from . import walker
 
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="tidy up your mess!")
+    parser.add_argument("source destination", type=str)
+
+if __name__ == "__main__":
+    main()
